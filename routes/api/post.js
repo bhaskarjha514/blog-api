ac73712a34e2775f13740a58ff45f31f011dac0a
Bhaskar Jha <35396885+bhaskarjha514@users.noreply.github.com>
2019-09-26 19:36:04 +0530

    Update post.js

--- routes/api/post.js
+++ routes/api/post.js
@@ -5,7 +5,7 @@ const Post = require('../../models/Post');
 router.get('/', (req, res, next)=>{
      Post.find()
          .then((posts)=>{
-             res.json(post);
+             res.json(posts);
          })
          .catch(err => console.log(err))
 });
